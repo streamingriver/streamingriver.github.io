9b0eaa75c2792be59e45b4678a35e020f6b0e8f5
Mindaugas Žvirblis <tinklapiai@gmail.com>
2019-05-07 13:55:50 +0300

    fix install-sr.sh

--- install-sr.sh
+++ install-sr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+echo "Installing GIT and cloning setup scripts. Please wait, setup will ask you to enter domain and ip address"
 cd /root
-yum install git-core -y –-quiet 
+yum install git-core -y --quiet 
 git clone https://github.com/streamingriver/setup-sr 
 cd setup-sr 
 ./setup-ansible.sh
